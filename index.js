6612ff0d51f76cb7aa40ef33353bd8bcd9f98a06
Christopher Santana <christopher@millionairepub.com>
2025-01-03 13:02:04 -0400

    updated cors stuff

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
 import express from 'express';
 import puppeteer from 'puppeteer-core';
-import chromium from "@sparticuz/chromium"
+import chromium from "@sparticuz/chromium";
+import cors from 'cors';
 
 const app = express();
 
-const port = 4000;
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/test-adbutler', async (req, res) => {
-
-    console.log("test-adbutler", await chromium.executablePath());
-    const browser = await puppeteer.launch({
-        executablePath: await chromium.executablePath(),
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-zygote',
-            '--single-process', // Recommended for Render
-            '--disable-gpu'
-        ],
-    });
-    const page = await browser.newPage();
-
-    // Navigate the page to a URL.
-    await page.goto('https://dainty-figolla-7c1184.netlify.app/', {
-        waitUntil: "networkidle0",
-    });
-
-    // Set screen size.
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Get the contents inside the div with #__nuxt id
-    const content = await page.evaluate(() => {
-        return document.querySelector('#__nuxt').innerHTML;
-    });
-
-    console.log({ content });
-
-    await browser.close();
-    // Return the content
-    res.status(200).send(content);
-
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
+app.get('/test-adbutler', async (req, res) => {
+    try {
+        console.log("test-adbutler", await chromium.executablePath());
+        const browser = await puppeteer.launch({
+            executablePath: await chromium.executablePath(),
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu'
+            ],
+        });
+        const page = await browser.newPage();
+
+        await page.goto('https://dainty-figolla-7c1184.netlify.app/', {
+            waitUntil: "networkidle0",
+        });
+
+        await page.setViewport({ width: 1080, height: 1024 });
+
+        const content = await page.evaluate(() => {
+            return document.querySelector('#__nuxt').innerHTML;
+        });
+
+        console.log({ content });
+
+        await browser.close();
+        res.status(200).send(content);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+const port = 4000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 
